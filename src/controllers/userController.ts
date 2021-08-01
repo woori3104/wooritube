6f431cb1a633d2ef31c4527616d1ed7aa2c4e7ad
woori3104 <sizuku11261@gmail.com>
2021-08-01 20:15:51 +0900

    ADD : controllers

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import * as express from "express"; 
 
-export const trending = (req:express.Request, res:express.Response)  => res.send("Home Page Videos");
-export const watch = (req:express.Request, res:express.Response) => res.send("Watch");
-export const edit = (req:express.Request, res:express.Response)  => res.send("Edit");
+export const join = (req:express.Request, res:express.Response) => res.send("Join");
+export const edit = (req:express.Request, res:express.Response) => res.send("Edit User");
+export const remove = (req:express.Request, res:express.Response) => res.send("Remove User");
