31176a47286021b641d2e8729791470e4f31f2df
woori3104 <sizuku11261@gmail.com>
2021-08-03 20:11:40 +0900

    ADD Pug Iteration

--- src/controllers/videoController.ts
+++ src/controllers/videoController.ts
@@ -1,7 +1,25 @@
 
 import * as express from "express"; 
 
-export const trending = (req:express.Request, res:express.Response) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "woori",
+  loggedIn: true,
+};
+export const trending = (req:express.Request, res:express.Response) => {
+  const videos = [
+    {
+      title: "Hello",
+    },
+    {
+      title: "Video #2",
+    },
+    {
+      title: "Whatsup",
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
+
 export const see = (req:express.Request, res:express.Response)  => res.render("watch");
 export const edit = (req:express.Request, res:express.Response) => res.render("edit");
 export const search = (req:express.Request, res:express.Response) => res.send("Search");
